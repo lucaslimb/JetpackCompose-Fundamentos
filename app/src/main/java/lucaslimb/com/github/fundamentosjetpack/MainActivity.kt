6645e227c7e9aea1c6fbb77394d7727d136283dd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-18 22:17:31 -0300

    Text e fonts

--- app/src/main/java/lucaslimb/com/github/fundamentosjetpack/MainActivity.kt
+++ app/src/main/java/lucaslimb/com/github/fundamentosjetpack/MainActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +31,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import lucaslimb.com.github.fundamentosjetpack.ui.theme.FundamentosJetpackTheme
 
@@ -35,20 +42,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             FundamentosJetpackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NotificationBage(Modifier.padding(innerPadding))
                     CardMensagem(Modifier.padding(innerPadding))
                 }
-            }
-        }
-    }
-
-    @Composable
-    fun Inicial(modifier: Modifier = Modifier) {
-        Column(modifier = modifier.background(color = Color.Blue)) {
-            TextoCustomizado("Primeira função composable.")
-            Row {
-                TextoCustomizado("Seja bem vindo!")
-                TextoCustomizado("Olá usuário.")
             }
         }
     }
@@ -60,25 +55,6 @@ class MainActivity : ComponentActivity() {
             color = Color.White,
             modifier = modifier
         )
-    }
-
-    @Composable
-    fun NotificationBage(modifier: Modifier = Modifier) {
-        Box(modifier = modifier.padding(16.dp), contentAlignment = Alignment.Center) {
-            Box(
-                modifier = Modifier
-                    .size(24.dp)
-                    .clip(CircleShape)
-                    .background(color = Color.Green)
-            )
-            Box(
-                modifier = Modifier
-                    .size(8.dp)
-                    .clip(CircleShape)
-                    .background(color = Color.Red)
-                    .align(Alignment.TopEnd)
-            )
-        }
     }
 
     @Composable
@@ -102,19 +78,61 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @Preview(showBackground = true)
     @Composable
-    fun PreviewFuncaoInicial() {
-        FundamentosJetpackTheme {
-            Inicial()
+    fun TesteButton() {
+        Button(
+            onClick = { /* TODO */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Green,
+                contentColor = Color.Black
+            ),
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            Text(text = "Confirmar")
         }
+    }
+
+    @Composable
+    fun TesteModifier() {
+        Column {
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+                    .border(border = BorderStroke(width = 1.dp,
+                        color = Color.Green))
+                    .padding(horizontal = 8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+                    .border(border = BorderStroke(width = 1.dp,
+                        color = Color.Green))
+                    .padding(horizontal = 8.dp, vertical = 8.dp)
+            )
+
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .border(border = BorderStroke(width = 1.dp,
+                        color = Color.Green))
+            )
+
+        }
+
     }
 
     @Preview(showBackground = true)
     @Composable
-    fun PreviewNotificationBage() {
+    fun PreviewFuncaoInicial() {
         FundamentosJetpackTheme {
-            NotificationBage()
+            TesteButton()
         }
     }
 
